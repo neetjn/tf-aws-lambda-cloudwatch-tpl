c0771973a8129c07179a72d804015f6c59d391b1
John Nolette <john.nolette@messagebird.com>
2022-10-10 17:49:06 -0400

    Terraform cleanup, adding archive module.

--- Makefile
+++ Makefile
@@ -5,3 +5,10 @@ build:
 
 tf:
 	docker-compose -f ${TF_COMPOSE} run --rm tf $(cmd)
+
+build+deploy:
+	yarn --cwd ./foobar-lambda run bundle
+	terraform -chdir=terraform init
+	terraform -chdir=terraform validate
+	terraform -chdir=terraform plan --out=plan
+	terraform -chdir=terraform apply plan
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.33.0"
@@ -8,7 +12,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -31,12 +35,19 @@ resource "aws_iam_role" "iam_for_lambda" {
   EOF
 }
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../foobar-lambda"
+  output_path = "lambda.js.zip"
+}
+
 resource "aws_lambda_function" "foobar_lambda" {
-  filename      = "lambda.js.zip"
-  function_name = "foobar"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda.handler"
-  runtime       = "nodejs16.x"
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "foobar"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda.handler"
+  runtime          = "nodejs16.x"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   ephemeral_storage {
     size = 10240 # Min 512 MB and the Max 10240 MB
