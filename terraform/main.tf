78f4150014227e0db1b93d4dfd82a8f3d8100e72
John Nolette <john.nolette@messagebird.com>
2022-10-10 20:26:09 -0400

    Adding README and cw logging for lambda.

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,8 +18,7 @@ provider "aws" {
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-  {
+  assume_role_policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
       {
@@ -31,14 +30,38 @@ resource "aws_iam_role" "iam_for_lambda" {
         "Sid": ""
       }
     ]
-  }
-  EOF
+  })
+}
+
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
 }
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "../foobar-lambda/dist/"
   output_path = "lambda.js.zip"
+}
+
+resource "aws_cloudwatch_log_group" "function_log_group" {
+  name              = aws_lambda_function.foobar_lambda.function_name
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_lambda_function" "foobar_lambda" {
@@ -52,4 +75,9 @@ resource "aws_lambda_function" "foobar_lambda" {
   ephemeral_storage {
     size = 10240 # Min 512 MB and the Max 10240 MB
   }
+}
+
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role = aws_iam_role.iam_for_lambda.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
 }
